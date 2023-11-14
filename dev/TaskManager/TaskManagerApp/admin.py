cd791d5c56965fb7ac5510351f69ddd6306dc5dc
elvfor <elvfor-0@student.ltu.se>
2023-11-14 10:31:14 +0100

    updated models + admin site

--- dev/TaskManager/TaskManagerApp/admin.py
+++ dev/TaskManager/TaskManagerApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Organization, Task, Project
+
+admin.site.register(Organization)
+admin.site.register(Task)
+admin.site.register(Project)
--- dev/TaskManager/TaskManagerApp/models.py
+++ dev/TaskManager/TaskManagerApp/models.py
@@ -1,31 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 from django.db import models
 
 class Organization(models.Model):
     organization_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)  # Add other organization-related details as needed
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    email = models.EmailField()
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
-    projects = models.ManyToManyField('Project')
+    #users = models.OneToManyField(User)
 
 
-class Admin(models.Model):
-    admin_id = models.AutoField(primary_key=True)
-    email = models.EmailField()
+#class User(models.Model):
+#    user_id = models.AutoField(primary_key=True)
+#    email = models.EmailField()
+#    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+#    projects = models.ManyToManyField('Project')
 
-class SuperUser(models.Model):
-    super_user_id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+#class Admin(models.Model):
+#    admin_id = models.AutoField(primary_key=True)
+#    email = models.EmailField()
+
+#class SuperUser(models.Model):
+#    super_user_id = models.AutoField(primary_key=True)
+#    user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Add other fields specific to the SuperUser if needed
 
 class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     project_name = models.CharField(max_length=255)
-    super_user = models.OneToOneField('SuperUser', on_delete=models.CASCADE)
+    #super_user = models.OneToOneField('SuperUser', on_delete=models.CASCADE)
 
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True)

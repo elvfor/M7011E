e85c88c7e56db4d19d903372968cdf40a357a665
jennysundstroem <ejyuso-2@student.ltu.se>
2023-11-14 15:48:55 +0100

    serializers under progress

---
+++ dev/TaskManager/TaskManagerApp/serializers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import User, Group
+from models import Organization, Task, Project, UserProfile
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+class OrganizationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Organization
+        fields = ['url', 'organization_id', 'name']
+
+class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['url', 'user', 'organization']
+
+class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['url', 'project_id', 'project_name', 'users']
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['url', 'task_id', 'name', 'description', 'status', 'estimated_time', 'label', 'project', 'users']

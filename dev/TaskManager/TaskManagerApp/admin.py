2e662c8eaea79b87c2ce84f270e0e4d18024f2e7
elvfor <elvfor-0@student.ltu.se>
2023-11-14 11:19:28 +0100

    updated models

--- dev/TaskManager/TaskManagerApp/admin.py
+++ dev/TaskManager/TaskManagerApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Organization, Task, Project
+from .models import Organization, Task, Project, UserProfile
 
 admin.site.register(Organization)
 admin.site.register(Task)
 admin.site.register(Project)
+admin.site.register(UserProfile)
+

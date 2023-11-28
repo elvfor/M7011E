786386e01ae5fe794dc8eb8509dd72a519b0553c
peggykhialie <peggy.khialie@gmail.com>
2023-11-28 13:31:50 +0100

    testing

--- dev/TaskManager/task/urls.py
+++ dev/TaskManager/task/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
 
     path('projects/<slug:project>/tasks/', TaskList.as_view(), name='task-list'),
-    path('tasks/<slug:slug>/', TaskDetail.as_view(), name='task-detail'),
+    #path('tasks/<slug:slug>/', TaskDetail.as_view(), name='task-detail'),
+    path('tasks/<slug:slug>/', views.TaskDetail.as_view(), name='task-detail'),
 
 ]
--- dev/TaskManager/task/views.py
+++ dev/TaskManager/task/views.py
@@ -1,9 +1,22 @@
 from .serializers import *
 from .models import Task
 from project.models import Project
-from rest_framework import generics, viewsets, permissions
+#from rest_framework import generics, viewsets, permissions
+from rest_framework import generics, viewsets, authentication, permissions
+
+class IsWorker(permissions.BasePermission):
+    message = {'detail': 'You must be a worker to do this.'}
+
+    def has_permission(self, request, view):
+        group_name = "Worker"  # Set your group name here
+        return request.user.groups.filter(name=group_name).exists()
+
+
 class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, (IsWorker | IsProjLeader | IsOrgLeader)]
+
     def get_queryset(self):
         #organization_slug = self.kwargs['organization']
         project_slug = self.kwargs['project']
@@ -13,7 +26,17 @@ class TaskList(generics.ListCreateAPIView):
         project = Project.objects.get(slug=self.kwargs['project'])
         serializer.save(project=project)
 
+    def get_queryset(self):
+        worker = self.request.user
+        return worker.tasks.all()
+
+
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
     lookup_field = 'slug'
     queryset = Task.objects.all()
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated, (IsWorker | IsProjLeader | IsOrgLeader)]
+
+
+

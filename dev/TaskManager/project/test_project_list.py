c13b3399a0327247514d9444c336bc631c90d64c
elvfor <elvfor-0@student.ltu.se>
2023-12-06 12:25:39 +0100

    added tests

--- dev/TaskManager/project/test_project_list.py
+++ dev/TaskManager/project/test_project_list.py
@@ -10,29 +10,67 @@ class ProjectListTest(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
+
+        #create users
         self.user_worker = User.objects.create_user(username='worker',
                                                     password='testpass123')
+        self.user_worker2 = User.objects.create_user(username='worker2',
+                                                     password='testpass123')
+        self.user_proj_leader = User.objects.create_user(username='proj_leader',
+                                                         password='testpass123')
+        self.user_org_leader = User.objects.create_user(username='org_leader',
+                                                        password='testpass123')
+        #create orgs + projs
         self.organization = Organization.objects.create(name='Test Organization',
                                                         slug='test-org')
-        self.organization.users.set([self.user_worker])
         self.project = Project.objects.create(name='Test Project',
                                               slug='test-project',
                                               organization=self.organization)
-        self.project.users.set([self.user_worker])
-        self.worker_group = Group.objects.create(name='Worker')
-        self.user_worker.groups.add(self.worker_group)
+        self.project2 = Project.objects.create(name='Test Project2',
+                                               slug='test-project2',
+                                               organization=self.organization)
+        #add certain users to orgs + projs
+        self.organization.users.add(self.user_worker)
+        self.organization.users.add(self.user_proj_leader)
 
-        self.client.force_authenticate(user=self.user_worker)
-        # self.url = reverse('projects:project-detail',
-        #                    kwargs={'slug': self.project.slug})
+        self.project.users.add(self.user_worker)
+        self.project2.users.add(self.user_worker)
+        self.project.users.add(self.user_proj_leader)
+
+        # create Groups and add users to said groups
+        self.worker_group = Group.objects.create(name='Worker')
+        self.proj_leader_group = Group.objects.create(name='Project Leader')
+        self.org_leader_group = Group.objects.create(name='Organization Leader')
+        self.user_worker.groups.add(self.worker_group)
+        self.user_worker2.groups.add(self.worker_group)
+        self.user_proj_leader.groups.add(self.proj_leader_group)
+        self.user_org_leader.groups.add(self.org_leader_group)
+
         self.url = f'/api/v1/organizations/{self.organization.slug}/projects/'
 
-    def test_retrieve_user_project_list(self):
-        # Make a GET request to retrieve the project detail
+    def test_get_project_list_worker_in_org(self):
+        """Test worker in org can get projects list"""
+        self.client.force_authenticate(user=self.user_worker)
         response = self.client.get(self.url)
-
-        # Check if the response status code is 200 OK
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # Check if the serialized data in the response matches the expected data
+        self.assertEqual(len(response.data), 2)
+    def test_get_project_list_worker_not_in_org(self):
+        """Test worker not in org can not get projects list"""
+        self.client.force_authenticate(user=self.user_worker2)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_project_list_org_leader_in_org(self):
+        """Test orgleader in org can not get projects list"""
+        self.client.force_authenticate(user=self.user_org_leader)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_project_list_proj_leader_in_org(self):
+        """Test proj leader in org can get projects list"""
+        self.client.force_authenticate(user=self.user_proj_leader)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         self.assertEqual(len(response.data), 1)

aad730de9ad679a98e40dfaa9fdc4734d0e73e4b
elvfor <elvfor-0@student.ltu.se>
2023-12-12 11:05:10 +0100

    updated user views

--- dev/TaskManager/user/urls.py
+++ dev/TaskManager/user/urls.py
@@ -6,5 +6,7 @@ app_name = 'user'
 urlpatterns = [
     path('user/create/', CreateUserView.as_view(), name='create'),
     path('user/token/', CreateTokenView.as_view(), name='token'),
-    path('user/update/', ManageUserView.as_view(), name='update'),
+    path('user/update', ManageUserView.as_view(), name='update'),
+    path('user/delete/', DeleteUserView.as_view(), name='delete'),
+    path('user/retrieve/', RetrieveUserView.as_view(), name='retrieve'),
 ]
